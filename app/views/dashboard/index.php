36207272f0eab80ba0bc2ad019eb192ead456c95
ChocoAvocado <albertuschristianp@gmail.com>
2022-05-10 08:15:52 +0700

    testing

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -129,7 +129,7 @@
             <!-- End of Content Wrapper -->
 
         </div>
-    </div>a
+    </div>
 
     <!-- Scroll to Top Button-->
     <a class="scroll-to-top rounded" href="#page-top">
