0f5212cd2fb87f77caa5f70346594d6f94d37e07
smartboss123 <wcahya72@gmail.com>
2022-06-11 23:09:27 +0700

    Pliss ya aku kerja 2 kali coy nek barang

--- app/views/webuser/barang/index.php
+++ app/views/webuser/barang/index.php
@@ -210,7 +210,7 @@
                 <div class="modal-body">Pilih "Logout" untuk keluar dari akun.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="/websitePeminjaman/public/logout"">Logout</a>
+                    <a class="btn btn-primary" href="/websitePeminjaman/public/logout">Logout</a>
                 </div>
             </div>
         </div>
