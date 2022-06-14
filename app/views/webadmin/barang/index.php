107e86109c08ab1fa5e2dd0e82899e4e2521e316
ChocoAvocado <albertuschristianp@gmail.com>
2022-06-14 14:58:45 +0700

    tambahan MULTILAB

--- app/views/webadmin/barang/index.php
+++ app/views/webadmin/barang/index.php
@@ -507,15 +507,14 @@
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="../index.php">Logout</a>
+                    <a class="btn btn-primary" href="logout">Logout</a>
                 </div>
             </div>
         </div>
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="<?= BASEURL; ?>/vendor/jquery/jquery.min.js"></script>
-    <script src="<?= BASEURL; ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  
 
     <!-- Core plugin JavaScript-->
     <script src="<?= BASEURL; ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
